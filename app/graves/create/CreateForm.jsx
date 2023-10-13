05e5b53f0469dafc04f12b86eeb0d95c212188b3
Son Roy Almerol <sonroyaalmerol@snry.home>
2023-10-13 19:53:13 -0400

    age calculation

--- app/graves/create/CreateForm.jsx
+++ app/graves/create/CreateForm.jsx
@@ -10,14 +10,25 @@ import {
   TextInput,
   FileInput
 } from "flowbite-react";
+import { useEffect, useState } from "react";
 
 export default function CreateForm() {
+  const [birth, setBirth] = useState(new Date());
+  const [death, setDeath] = useState(new Date());
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    const diff = Math.abs(new Date(death) - new Date(birth));
+    const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+    setAge(years);
+  }, [birth, death]);
+
   return (
-    <div className="flex justify-center items-center my-4">
+    <div className="flex items-center justify-center my-4">
       <Card className="w-4/5 max-w-sm">
         <form action={addGrave} className="grid grid-cols-2 gap-4">
           <div className="max-w-md" id="fileUpload">
-            <div className="mb-2 block">
+            <div className="block mb-2">
               <Label htmlFor="file" value="Upload file" />
             </div>
             <FileInput
@@ -28,21 +39,17 @@ export default function CreateForm() {
             />
           </div>
           <div className="max-w-md col-span-2" id="select">
-            <div className="mb-2 block">
+            <div className="block mb-2">
               <Label htmlFor="cemeteries" value="Select your cemetery" />
             </div>
             <Select name="cemetery" id="cemeteries" required>
-              <optgroup id="region" label="Davao Del Sur">
-                <optgroup id="city" label="Davao City">
-                  <option>Cemetery 1</option>
-                  <option>Cemetery 2</option>
-                  <option>Cemetery 3</option>
-                  <option>Cemetery 4</option>
-                </optgroup>
-              </optgroup>
+              <option>Cemetery 1</option>
+              <option>Cemetery 2</option>
+              <option>Cemetery 3</option>
+              <option>Cemetery 4</option>
             </Select>
           </div>
-          <div class="relative z-0 w-full group block">
+          <div className="relative z-0 block w-full group">
             <TextInput
               type="text"
               name="firstname"
@@ -52,7 +59,7 @@ export default function CreateForm() {
               required
             />
             <Label
-              htmlfor="first_name"
+              htmlFor="first_name"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               First name
@@ -68,13 +75,13 @@ export default function CreateForm() {
               required
             />
             <Label
-              htmlfor="last_name"
+              htmlFor="last_name"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Last name
             </Label>
           </div>
-          <div class="relative z-0 w-full group col-span-2">
+          <div className="relative z-0 w-full col-span-2 group">
             <TextInput
               type="text"
               name="aliases"
@@ -83,22 +90,24 @@ export default function CreateForm() {
               placeholder=" "
             />
             <Label
-              htmlfor="aliases"
+              htmlFor="aliases"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Aliases
             </Label>
           </div>
-          <div class="relative z-0 w-full group col-span-2">
+          <div className="relative z-0 w-full col-span-2 group">
             <TextInput
               type="text"
               name="age"
               id="age"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               placeholder=" "
+              value={age}
+              readOnly
             />
             <Label
-              htmlfor="age"
+              htmlFor="age"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Age
@@ -106,18 +115,51 @@ export default function CreateForm() {
           </div>
           <div className="col-span-2">
             <div className="relative z-30 w-full mb-6 group">
-              <Datepicker name="birth" id="birthpicker" title="Birth" />
+              <TextInput
+                type="date"
+                name="birth"
+                id="birthpicker"
+                title="Birth"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=""
+                onChange={e => {
+                  setBirth(e.target.value);
+                }}
+              />
+              {/*
+              <Datepicker
+                name="birth"
+                id="birthpicker"
+                title="Birth"
+                onSelectedDateChanged={date => { console.log(date) }}
+              />
+              */}
               <Label
-                htmlfor="birthpicker"
+                htmlFor="birthpicker"
                 className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Birth
               </Label>
             </div>
             <div className="relative z-20 w-full mb-6 group">
-              <Datepicker name="death" id="deathpicker" title="Death" />
+              <TextInput
+                type="date"
+                name="death"
+                id="deathpicker"
+                title="Death"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=""
+                onChange={e => {
+                  setDeath(e.target.value);
+                }}
+              />
+              {/*<Datepicker
+                name="death"
+                id="deathpicker"
+                title="Death"
+              />*/}
               <Label
-                htmlfor="deathpicker"
+                htmlFor="deathpicker"
                 className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Death
@@ -130,13 +172,13 @@ export default function CreateForm() {
                 title="Internment"
               />
               <Label
-                htmlfor="internmentpicker"
+                htmlFor="internmentpicker"
                 className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Internment
               </Label>
             </div>
-            <div class="relative z-0 w-full group col-span-2">
+            <div className="relative z-0 w-full col-span-2 group">
               <TextInput
                 type="text"
                 name="location"
@@ -146,7 +188,7 @@ export default function CreateForm() {
                 required
               />
               <Label
-                htmlfor="location"
+                htmlFor="location"
                 className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Add Location Longitude and Latitude
