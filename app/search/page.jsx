2c645c4c7a7cff87d63fdf4e1462b6bef71eeb67
Son <github@snry.me>
2024-02-12 19:05:20 -0500

    Update page.jsx

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -122,21 +122,6 @@ export default function Search() {
               Age (Fixed)
             </Label>
           </div>
-          <div className="relative z-0 w-full group col-span-2">
-            <TextInput
-              type="text"
-              name="agerange"
-              id="floating_last_name"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-            />
-            <Label
-              htmlFor="agerange"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Age Range
-            </Label>
-          </div>
           <div className="relative z-20 w-full mb-6 group">
             <TextInput type="number" name="age_min" id="age_min" title="Age Min (Range)" />
             <Label
