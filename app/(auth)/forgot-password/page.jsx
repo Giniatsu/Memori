3982bac599b026e35778b520d4bf4c73ce3cb1e2
Son Roy Almerol <github@snry.me>
2024-02-25 14:13:31 -0500

    initial forgot password implementation + move auth page redirects to middleware

---
+++ app/(auth)/forgot-password/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import ForgotPasswordForm from "../ForgotPasswordForm";
+
+export default function ForgotPassword() {
+  return (
+    <main>
+      <h5 className="text-center text-xl my-4">Forgot Password</h5>
+      <ForgotPasswordForm />
+    </main>
+  );
+}
