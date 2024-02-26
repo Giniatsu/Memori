68b03692f3a90621b238759a395c6865f67a373d
Son <github@snry.me>
2024-02-26 09:34:08 -0500

    debug: remove buttons

--- app/search/results/SearchList.jsx
+++ app/search/results/SearchList.jsx
@@ -142,24 +142,6 @@ export default async function GravesList() {
         </Link>
       ))}
       {graves.length === 0 && <p className="text-center">No Graves</p>}
-      <div className="flex justify-center mt-4">
-        { currentPage !== 1 && (
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => setCurrentPage((prevPage) => prevPage - 1)}
-          >
-            Previous Page
-          </button>
-        ) }
-        { !(graves.length < pageSize || currentPage * pageSize >= totalCount) && (
-          <button
-            className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => setCurrentPage((prevPage) => prevPage + 1)}
-          >
-            Next Page
-          </button>
-        ) }
-      </div>
     </>
   );
 }
