67fd8f81a4f928360c131c9223af4b27fc5aa414
Giniatsu <aragarcia@addu.edu.ph>
2024-03-07 16:02:02 +0800

    Temporary edit on carousel

--- app/graves/contributions/GraveImage.jsx
+++ app/graves/contributions/GraveImage.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Image from "next/image";
+import { Carousel } from "flowbite-react";
 
 const BASE_URL = 'https://plmqhcualnnsirfqjcsj.supabase.co/storage/v1/object/public/grave_images/';
 
@@ -40,21 +41,24 @@ export default function GraveImage({ grave_id, multiple }) {
   }, [grave_id])
  
   if (multiple) {
-    return !loading && (
-      <>
-        { imageUrls?.map((imageUrl) => (
-          <Image
-            key={imageUrl}
-            src={imageUrl ?? ""}
-            alt=""
-            height={384}
-            width={384}
-            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-          />
-        ))
-        }
-      </>
-    )
+    return (
+      !loading && (
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+          <Carousel>
+            {imageUrls?.map((imageUrl) => (
+              <Image
+                key={imageUrl}
+                src={imageUrl ?? ""}
+                alt=""
+                height={384}
+                width={384}
+                className=""
+              />
+            ))}
+          </Carousel>
+        </div>
+      )
+    );
   }
 
   return !loading && (imageUrls && imageUrls.length > 0) && (
