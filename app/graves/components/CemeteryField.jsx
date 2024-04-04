295878d3c1f3641735cc654d471ff5651818047c
Giniatsu <aragarcia@addu.edu.ph>
2024-04-04 11:59:46 +0800

    Update cemetery address ID in CemeteryField component

--- app/graves/components/CemeteryField.jsx
+++ app/graves/components/CemeteryField.jsx
@@ -72,7 +72,7 @@ const CemeteryField = ({
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             />
             <Textarea
-              id="cemeteryaddress"
+              id="cemetery_address"
               type="text"
               rows={4}
               name="cemetery_address"
