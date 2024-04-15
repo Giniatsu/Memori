9c0f54255935ebdfe99377b998c2a931becee760
Giniatsu <aragarcia@addu.edu.ph>
2024-04-16 00:46:39 +0800

    Update cemetery location placeholders and labels

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -115,7 +115,7 @@ export default function Search() {
                 name="cemeterylocation"
                 id="cemetery_location"
                 className="block pt-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder="Search for a cemetery location (City, State, Country)"
+                placeholder="Cemetery location (City or State)"
                 onChange={(e) => {
                   setHasLocation(false);
                 }}
