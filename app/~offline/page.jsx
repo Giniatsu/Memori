c0db3755002fc3c718467640802a935b4aa7acd5
Giniatsu <aragarcia@addu.edu.ph>
2024-04-28 17:09:23 +0800

    updated text on fallback

--- app/~offline/page.jsx
+++ app/~offline/page.jsx
@@ -4,7 +4,7 @@ const OfflinePage = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800">
       {/* Your page content below */}
-      <div className="container mx-auto p-8">
+      <div className="container mx-auto p-8 text-pretty">
         <h1 className="text-3xl font-bold mb-4">Whoops! Offline Mode</h1>
         <p className="text-center text-lg mb-8">
           Looks like your internet connection took a break. Don&apos;t worry, we&apos;ll
