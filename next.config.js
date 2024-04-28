3304121f3ff23541a25a76294afb0163f14dee66
Giniatsu <aragarcia@addu.edu.ph>
2024-04-28 17:34:04 +0800

    Add cacheOnFrontEndNav option to next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   fallbacks: {
     document: "/~offline",
-  }
+  },
+  cacheOnFrontEndNav: true,
 });
 const nextConfig = {}
 
